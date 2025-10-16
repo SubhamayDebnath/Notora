3c22f4753931b90cee002250e5bd20e25c7d01fc
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-10-16 21:47:51 +0530

    remove client folder

--- client/src/components/home/sidebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sidebar = () => {
-  return (
-    <div className='w-32 min-h-[calc(100dvh-10rem)] h-full'>Sidebar</div>
-  )
-}
-
-export default Sidebar
--- client/src/pages/home/homePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Homepage = () => {
-  return (
-    <div className='w-full p-5'>Homepage</div>
-  )
-}
-
-export default Homepage
