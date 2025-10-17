21e0226818d8960bf85c31fa83e153be273e4491
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-10-17 20:26:33 +0530

    feat: implement addCategory controller in category controller

---
+++ server/src/controllers/category.controller.js
@@ -0,0 +1,7 @@
+import asyncHandler from "../utils/asyncHandler.js";
+import AppError from "../utils/appError.js";
+import ApiResponse from "../utils/apiResponse.js";
+
+// add new category
+
+export const addCategory = asyncHandler(async (req, res) => {});
